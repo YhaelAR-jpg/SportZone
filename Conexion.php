3b47a312175ab82946d7ea02876e80a648e8564c
YhaelAR-jpg <2319039090@upfim.edu.mx>
2026-03-10 10:26:46 -0600

    Update Conexion.php

--- Conexion.php
+++ Conexion.php
@@ -1,14 +1,13 @@
 <?php
-//Declaración de variables para conexión
-$usuario = "root";
-$password = ""; 
 $servidor = "localhost";
-$baseDeDatos = "tiendadeportes";
+$usuario = "root";
+$password = "";
+$baseDeDatos = "TiendaDeportes";
 
-//Conexión del servidor 
-$conexion = mysqli_connect($servidor, $usuario, $password) or die ( "No se puede conectar al servidor, revisa la contraseña o usuario");
+$conexion = mysqli_connect($servidor, $usuario, $password, $baseDeDatos);
 
-//Conexión a la base de datos 
-$bd =mysqli_select_db($conexion, $baseDeDatos) or die ("No se establecio el acceso a la base de datos");
+if (!$conexion) {
+    die("Error de conexión: " . mysqli_connect_error());
+}
 ?>
 
